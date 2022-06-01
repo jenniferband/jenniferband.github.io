8192f747f5da634f4f3545d7fc1e9f0a9eb904cd
Tiina Partanen <tyynetyyne@gmail.com>
2022-06-01 09:33:46 +0300

    react demo

---
+++ alkeet/precache-manifest.af37d5a1bb1a77012b8add1a6a6adc76.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5df97669f6e52d1faab786746585d5e",
+    "url": "/alkeet/index.html"
+  },
+  {
+    "revision": "954a1fdebc7a2b47c116",
+    "url": "/alkeet/static/css/main.5e79661d.chunk.css"
+  },
+  {
+    "revision": "96405a5f11a36008d138",
+    "url": "/alkeet/static/js/2.74b1dee2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/alkeet/static/js/2.74b1dee2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "954a1fdebc7a2b47c116",
+    "url": "/alkeet/static/js/main.d98917f4.chunk.js"
+  },
+  {
+    "revision": "2f693484a3120ae9bac2",
+    "url": "/alkeet/static/js/runtime-main.46de3334.js"
+  }
+]);
